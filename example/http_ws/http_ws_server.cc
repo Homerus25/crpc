09663aed85392a6fa5140d40c77417f15e4f0383
homerus <s.schwarzkopf@posteo.net>
2023-05-18 19:40:05 +0200

    http-ws-server has concurrency parameter

--- example/http_ws/http_ws_server.cc
+++ example/http_ws/http_ws_server.cc
@@ -5,5 +5,13 @@ int main(int argc, char* argv[]) {
   boost::asio::io_context ioc;
   http_ws_server<benchmark_interface> server{ioc};
   register_benchmark_interface(server);
+
+  auto threads_count = argc == 2 ? std::atoi(argv[1]) : 1;
+  std::vector<std::thread> thread_pool;
+  for(int i=0; i<threads_count-1; ++i)
+    thread_pool.emplace_back([&](){ ioc.run(); });
+
   ioc.run();
+  for (auto& thread : thread_pool)
+    thread.join();
 }
--- example/mqtt/mqtt-server.cc
+++ example/mqtt/mqtt-server.cc
@@ -4,6 +4,6 @@
 int main(int argc, char* argv[]) {
   auto server = rpc_mqtt_server<benchmark_interface>(2000);
   register_benchmark_interface(server);
-  server.run(std::atoi(argv[1]));
+  server.run(argc == 2 ? std::atoi(argv[1]) : 1);
 }
 
